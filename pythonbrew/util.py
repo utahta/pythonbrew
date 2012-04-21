9dd25be48920f1394dc71412838aaec4e6843ea0
utahta <labs.ninxit@gmail.com>
2012-04-21 20:09:55 +0900

    fixed issue #68

--- pythonbrew/util.py
+++ pythonbrew/util.py
@@ -57,8 +57,9 @@ def is_html(content_type):
 
 def is_gzip(content_type, filename):
     if(content_type == 'application/x-gzip'
-       or splitext(filename)[1].lower() in ('.tar', '.tar.gz', '.tgz')
-       or tarfile.is_tarfile(filename)):
+       or splitext(filename)[1].lower() in ('.tar', '.tar.gz', '.tgz')):
+        return True
+    if os.path.exists(filename) and tarfile.is_tarfile(filename):
         return True
     return False
 
