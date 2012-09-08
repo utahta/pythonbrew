245543afe7e9f4ce9cb325041786f8961d5b2822
Qian Long <ourbaozi@qq.com>
2012-09-09 05:55:57 +0800

    add new patch

--- pythonbrew/installer/pythoninstaller.py
+++ pythonbrew/installer/pythoninstaller.py
@@ -138,6 +138,9 @@ class PythonInstaller(object):
             if version < '2.7.2':
                 patch_dir = os.path.join(PATH_PATCHES_ALL, "common")
                 self._add_patches_to_list(patch_dir, ['patch-setup.py.diff'])
+            if version == '2.7.3':
+                patch_dir = os.path.join(PATH_PATCHES_ALL, "python27")
+                self._add_patches_to_list(patch_dir, ['patch-Modules-_sqlite-connection.c.diff'])
         elif is_python30(version):
             patch_dir = os.path.join(PATH_PATCHES_ALL, "python30")
             self._add_patches_to_list(patch_dir, ['patch-setup.py.diff'])
